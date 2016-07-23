1861582f08f4b64e2b6aef8df23e42d1a4e0536f
sliterod <xliterodzero@gmail.com>
2016-07-23 15:15:33 -0400

    Animaciones
    Carpeta de animaciones

--- Assets/Scripts/ColliderEvents/ClickEvents.cs
+++ Assets/Scripts/ColliderEvents/ClickEvents.cs
@@ -5,6 +5,11 @@ public class ClickEvents : MonoBehaviour {
     void OnMouseDown() {      
         if (Input.GetMouseButtonDown(0)) {
             Debug.Log("Click on element");
+
+            Animator animator;
+            animator = this.GetComponent<Animator>();
+
+            animator.SetBool("isPlaying", true);
         }
     }
 }
