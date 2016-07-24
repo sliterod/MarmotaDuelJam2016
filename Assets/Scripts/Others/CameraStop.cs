3c903a2a5310922b96abf1e1f59053cadcc9b2e3
adsith <carlosalvarez1512@gmail.com>
2016-07-24 21:16:03 +0200

    Run Completo

---
+++ Assets/Scripts/Others/CameraStop.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraStop : MonoBehaviour {
+
+    public CamFollow camera;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D triggerCollider)
+    {
+        camera.CanFollowCharacter = false;
+    }
+}
